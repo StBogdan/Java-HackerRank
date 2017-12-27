d4fb0cf4455b92947153b9deefec9c97135140fd
StBogdan <bogdan.stoicescu95@gmail.com>
2017-12-27 12:16:24 +0200

    Added rest of solutions for Introduction subdomain

---
+++ src/com/company/Introduction/Java_CurrencyFormatter.java
@@ -0,0 +1,29 @@
+package com.company.Introduction;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Java_CurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat franceFormat     = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        NumberFormat indiaFormat     = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+
+        String us = usFormat.format(payment);
+        String china = chinaFormat.format(payment);
+        String france= franceFormat.format(payment);
+        String india   = indiaFormat.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
